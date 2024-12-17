65c0606ce4faef16d1a0e91f016eface515bf349
heKim <khu4237@gmail.com>
2024-12-17 17:40:04 +0900

    init commit

---
+++ src/main/java/study/memeback/MemeBackApplication.java
@@ -0,0 +1,13 @@
+package study.memeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MemeBackApplication.class, args);
+  }
+
+}
---
+++ src/test/java/study/memeback/MemeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package study.memeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeBackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
